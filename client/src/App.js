e25ff0dcdb0aae1b2a2876d1f6abf009bf061b3b
Shan Wong <shan.wong29@gmail.com>
2019-12-03 19:18:18 +0100

    initialize project

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./App.css";
+import { Route, Redirect } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
+class App extends React.Component {
+  state = {
+    user: this.props.user
+  };
+
+  setUser = user => {
+    this.setState({
+      user: user
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar user={this.state.user} clearUser={this.setUser} />
+
+        <Route
+          exact
+          path="/signup"
+          // component={Signup}
+          render={props => <Signup {...props} setUser={this.setUser} />}
+        />
+        <Route
+          exact
+          path="/login"
+          render={props => <Login {...props} setUser={this.setUser} />}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
