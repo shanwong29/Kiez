0d71aeb6f0bca69797a02915173414422ec1b98f
Shan Wong <shan.wong29@gmail.com>
2019-12-10 18:21:16 +0100

    deploy to heroku

---
+++ client/build/precache-manifest.bfe6dbe368679d0a206e430fe7579e7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d36dd72b338791f4b56a6b98f5aa7b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf736dc1af4fe7368b38",
+    "url": "/static/css/2.a1f70397.chunk.css"
+  },
+  {
+    "revision": "5704e5e6a30db7a7e9c3",
+    "url": "/static/css/main.b40799ca.chunk.css"
+  },
+  {
+    "revision": "bf736dc1af4fe7368b38",
+    "url": "/static/js/2.a97ae14f.chunk.js"
+  },
+  {
+    "revision": "5704e5e6a30db7a7e9c3",
+    "url": "/static/js/main.94bd9463.chunk.js"
+  },
+  {
+    "revision": "18ceee0904d39392a2a3",
+    "url": "/static/js/runtime-main.8095ec4f.js"
+  }
+]);
