3fe24ca272ded8da093e0a3eda10424128194121
Shan Wong <shan.wong29@gmail.com>
2019-12-10 18:34:48 +0100

    heroku deploy

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // ADD SESSION SETTINGS HERE:
@@ -111,5 +111,10 @@ app.use("/api/auth", authRoutes);
 
 const chatRoutes = require("./routes/chat");
 app.use("/api/chat", chatRoutes);
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 module.exports = app;
